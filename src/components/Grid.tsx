0635d8c943c78fe2b219123f9bba76785c0fdb3e
Nobeen Islam <40375177+NobeenIslam@users.noreply.github.com>
2022-05-14 15:24:54 +0100

    Created 5 by 5 grid which takes each letter of the guess input

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
 import { Guess } from "./Guess";
 
 export function Grid(): JSX.Element {
-  return <Guess />;
+  const [guess, setGuess] = useState<string>("");
+  console.log(guess);
+  return (
+    <section>
+      <input
+        placeholder="Put your guess here"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value)}
+        maxLength={5}
+        className="guessBar"
+      ></input>
+      `
+      <Guess guess={guess} />
+      <Guess guess={guess} />
+      <Guess guess={guess} />
+      <Guess guess={guess} />
+      <Guess guess={guess} />
+    </section>
+  );
 }
